490ecef8571a9a6d85fcf6d1e16f6f2b4ddf6343
Your Name <hoangducvinh04102001@gmail.com>
2022-05-15 22:27:35 +0700

    add Header

---
+++ src/Body.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+    <div id="conten">
+            <div id="conten" class="col-md-6 col-lg-8 row">
+                <div class="item ">
+                
+                    <p class="item_title">Hoa Sinh Nhật</p>
+                    <button class="add_basket">Mua Ngay</button>
+                </div>
+                <div class="item ">
+
+                    <p class="item_title">Hoa Tặng</p>
+                    <button class="add_basket">Mua Ngay</button>
+                </div>
+                <div class="item ">
+
+                    <p class="item_title">Hoa Cưới</p>
+                    <button class="add_basket">Mua Ngay</button>
+                </div>
+                <div class="item ">
+
+                    <p class="item_title">Hoa Khai Trương</p>
+                    <button class="add_basket">Mua Ngay</button>
+                </div>
+                <div class="item ">
+
+                    <p class="item_title">Hoa Tỏ Tình</p>
+                    <button class="add_basket">Mua Ngay</button>
+                </div>
+                <div class="item ">
+
+                    <p class="item_title">Hoa Khô</p>
+                    <button class="add_basket">Mua Ngay</button>
+                </div>
+            </div>
+        </div>
+  )
+}
